7412ec0661f3e9ceb9ef06d0f789a5049755e3d8
webroyit <xytank@gmail.com>
2024-06-07 15:36:01 -0400

    Name contract

--- Simple Storage/SimpleStorage.sol
+++ Simple Storage/SimpleStorage.sol
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity 0.8.18;     
 // pragma solidity ^0.8.18;             // Add carrot ^ for this contract to work with 0.8.18 or greater 
-// pragma solidity >=0.8.18 <0.9.0;     // Any version between these two is a valid version for this contract
+// pragma solidity >=0.8.18 <0.9.0;    // Any version between these two is a valid version for this contract
 
+contract SimpleStorage {
+    
+}
 
 // Notes
 // --------------
