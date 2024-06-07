836e6fbf5168876d08bf06420d98a3d117e067ce
webroyit <xytank@gmail.com>
2024-06-07 13:59:29 -0400

    Set Solidity Version

---
+++ Simple Storage/SimpleStorage.sol
@@ -0,0 +1,8 @@
+pragma solidity 0.8.18;     
+// pragma solidity ^0.8.18;             // Add carrot ^ for this contract to work with 0.8.18 or greater 
+// pragma solidity >=0.8.18 <0.9.0;     // Any version between these two is a valid version for this contract
+
+
+// Notes
+// --------------
+// - 
