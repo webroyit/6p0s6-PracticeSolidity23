df612b73b3605cf3132b07ada8cafa25cef9739d
webroyit <xytank@gmail.com>
2024-06-08 14:07:35 -0400

    Function

--- Simple Storage/SimpleStorage.sol
+++ Simple Storage/SimpleStorage.sol
@@ -5,12 +5,18 @@ pragma solidity 0.8.18;
 
 contract SimpleStorage {
     // Basic Types: boolean, uint, int, address, bytes
-    bool hasFavoriteNumber = true;
-    uint favoriteNumber = 88;
-    string favoriteNumberInText = "eighty-eight";
-    int favoriteInt = -88;
-    address myAddress = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
-    bytes32 favoriteBytes32 = "cat";
+    // bool hasFavoriteNumber = true;
+    // uint favoriteNumber = 3;
+    // string favoriteNumberInText = "eighty-eight";
+    // int favoriteInt = -88;
+    // address myAddress = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    // bytes32 favoriteBytes32 = "cat";
+
+    uint256 public favoriteNumber;     // By default is 0 if not value is given
+    
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
 }
 
 // Notes
@@ -20,3 +26,5 @@ contract SimpleStorage {
 // - Variables are holders for different values
 // - Not specify the uint it defaults to being uint 256
 // - int can be positive or negative number
+// - Visibility for variables default to internal
+// - Everything on EVM chains is public data
