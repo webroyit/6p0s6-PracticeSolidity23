86afeb299e1246ac9732be86cff28f0f529fd350
webroyit <xytank@gmail.com>
2024-06-08 13:11:17 -0400

    Basic Types

--- Simple Storage/SimpleStorage.sol
+++ Simple Storage/SimpleStorage.sol
@@ -4,9 +4,19 @@ pragma solidity 0.8.18;
 // pragma solidity >=0.8.18 <0.9.0;    // Any version between these two is a valid version for this contract
 
 contract SimpleStorage {
-    
+    // Basic Types: boolean, uint, int, address, bytes
+    bool hasFavoriteNumber = true;
+    uint favoriteNumber = 88;
+    string favoriteNumberInText = "eighty-eight";
+    int favoriteInt = -88;
+    address myAddress = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    bytes32 favoriteBytes32 = "cat";
 }
 
 // Notes
 // --------------
 // - It is recommended to add SPDX-License
+// - Bytes is a low level type
+// - Variables are holders for different values
+// - Not specify the uint it defaults to being uint 256
+// - int can be positive or negative number
