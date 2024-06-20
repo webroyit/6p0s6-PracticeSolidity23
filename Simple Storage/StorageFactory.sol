9f3d77431fdd6cf53c054d34a2fb9e6f0fcf0037
webroyit <xytank@gmail.com>
2024-06-20 12:15:23 -0400

    Import Contract

--- Simple Storage/StorageFactory.sol
+++ Simple Storage/StorageFactory.sol
@@ -1,31 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;   
 
-contract SimpleStorage {
-    uint256 myfavoriteNumber;
-
-    struct Person {
-        uint256 favoriteNumber;
-        string name;
-    }
-
-    Person[] public listOfPeople;
-    mapping(string => uint256) public nameToFavoriteNumber;
-
-    function store(uint256 _favoriteNumber) public {
-        myfavoriteNumber = _favoriteNumber;
-    }
-
-    function retrieve() public view returns(uint256) {
-        return myfavoriteNumber;
-    }
-
-    function addPerson(string memory _name, uint256 _favoriteNumber) public {
-        listOfPeople.push( Person(_favoriteNumber, _name));
-
-        nameToFavoriteNumber[_name] = _favoriteNumber;
-    }
-}
+import "/SimpleStorage.sol";
 
 contract StorageFactory {
     // type visibility name
