f340aa9d173979094e6afe0195c952bc58d42fc4
webroyit <xytank@gmail.com>
2024-07-02 13:22:49 -0400

    Inheritance

---
+++ Simple Storage/AddFiveStorage.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+import {SimpleStorage} from "./SimpleStorage.sol";
+
+// Inheritance everything from SimpleStorage
+contract AddFiveStorage is SimpleStorage {
+
+}
