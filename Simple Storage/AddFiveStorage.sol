4988be24bc83fc7758e43c0fbf4349d8543b8efe
webroyit <xytank@gmail.com>
2024-07-02 14:12:06 -0400

    Pure Function

--- Simple Storage/AddFiveStorage.sol
+++ Simple Storage/AddFiveStorage.sol
@@ -5,5 +5,7 @@ import {SimpleStorage} from "./SimpleStorage.sol";
 
 // Inheritance everything from SimpleStorage
 contract AddFiveStorage is SimpleStorage {
-
+    function sayHello() public pure returns(string memory) {
+        return "Hello";
+    }
 }
